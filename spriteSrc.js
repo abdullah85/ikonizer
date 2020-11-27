25a020549af8bbe3a109f9cc11700bb9bc646330
Bitwiser Dev <bitwiseviews@gmail.com>
2020-11-27 09:29:35 +0530

    info lines added to each command.

--- spriteSrc.js
+++ spriteSrc.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2020 bitwiseviews --- https://bitwiseviews.github.io/
+Copyright (c) 2020 bitwiseviews --- https://bitwiseviews.github.io/ikonizer
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- symbolTracer.js
+++ symbolTracer.js
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2020 bitwiseviews --- https://bitwiseviews.github.io/ikonizer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 const symbParser = new DOMParser();
 const moveTo = ['M', 'm'];
 const line = ['L', 'l', 'H', 'h', 'V', 'v'];
@@ -10,12 +34,28 @@ const commands = [...moveTo, ...line, ...cubic, ...quad, ...ellArc, ...closeP];
 let commandSeq = "";
 let commandSeqIdL=[];
 let lineNo = -1;
+let commandL = [];
+let opL = [];
+let paramL = [];
+// To illustrate information about the movement visually...
+let infoL = [];
+// initial coordinates before the current operation...
+// Should be updated after each operation...
+let initX=0, initY = 0;
+// Store the coords obtained as a result of each operation as an object
+let coordsL = [];
 
 function traceSymbol(){
     // First, reset commandSeq, commandSeqIdL, lineNo
     commandSeq = "";
     commandSeqIdL = [];
     lineNo = -1;
+    commandL = [];
+    opL = [];
+    paramL = [];
+    coordsL = [];
+    infoL = [];
+
     let srcElem = document.getElementById('srcArea');
     let srcNode = symbParser.parseFromString(srcElem.innerText,"image/svg+xml");
     let srcSymbol = srcNode.childNodes[0];
@@ -40,14 +80,13 @@ function traceSymbol(){
     /*
      * First objective is to get a canonical representation for data as a list of commands (with parameters)
      */
-    let commandL = [];
-    let opL = [];
-    let paramL = [];
     let re = commands.reduce( (expr, cmd) => expr === "" ? cmd : expr+"|" + cmd, "");
     let commandExpr = new RegExp(re, 'g');
     let currIdx = data.search(commandExpr);
     let offset = data.slice(currIdx+1).search(commandExpr);
+    let prevCoords = {'x' : 0, 'y' : 0};
     let params = "";
+    let nextCoords = {'x' : 0, 'y': 0};
     while(offset >= 0){
 	offset = data.slice(currIdx+1).search(commandExpr);
 	let nextIdx = (currIdx + 1) + offset;
@@ -67,7 +106,13 @@ function traceSymbol(){
 	commandL.push(currCmd);
 	commandSeq = commandSeq + currCmd + "\n";
 	commandSeqIdL.push(commandSeq.length);
+	nextCoords = getNextCoords(prevCoords, operation, paramsVal);
+	coordsL.push(nextCoords);
+	let info = getInfoFor(operation, paramsVal, prevCoords, nextCoords);
+	infoL.push(info);
 	currIdx = nextIdx;
+	prevCoords['x'] = nextCoords['x'];
+	prevCoords['y'] = nextCoords['y'];
     }
     commandL.push(data.slice(currIdx, currIdx+1));
 
@@ -79,6 +124,72 @@ function traceSymbol(){
     trBody += getTracerButtonSrc("tracerPause","||");
     trBody += getTracerButtonSrc("tracerNext",">>");
     targetArea.innerHTML += "<div class=\"break\"></div>"+ trHdr + trBody + trFtr;
+}
+
+function getNextCoords(prevCoords, operation, params){
+    prevX = prevCoords['x'];
+    prevY = prevCoords['y'];
+    if(operation.match(/z/i)) // closepath ... return current coordinates for now (needs to be corrected)
+	return {'x':initX, 'y':initY}; // return global initX, initY values
+    if(operation.match(/M|L/)){
+	if(operation == "M") {
+	    initX = params[0];
+	    initY = params[1];
+	}
+	return {'x':params[0], 'y':params[1]};
+    }
+    if(operation.match(/m|l/)){
+	if(operation == "m") {
+	    initX = prevX + params[0];
+	    initY = prevY + params[1];
+	}
+	return {'x':(prevX + params[0]), 'y':(prevY + params[1])};
+    }
+    if(operation.match(/c|C/)){
+	if(operation == "c")
+	    return {'x' : (prevX + params[4]), 'y':(prevY + params[5])};
+	// else operation == "C"
+	return {'x' : (params[4]), 'y':(params[5])};
+    }
+
+    return {'x':prevCoords['x'], 'y':prevCoords['y']};
+}
+
+/*
+  Visually draw the required sketches to better follow current operation with parameters
+*/
+function getInfoFor(operation, params, prevCoords, nextCoords){
+    let cmds = "";
+    let prevX = prevCoords['x'];
+    let prevY = prevCoords['y'];
+    let nextX = nextCoords['x'];
+    let nextY = nextCoords['y'];
+    if(operation.match(/z/i)) {// closepath ...
+	cmds += "<circle cx=\""+prevX+"\" cy=\""+prevY+"\" r=\"5\" fill=\"red\"/>\n";
+	cmds += "<line style =\"stroke:rgb(155, 155, 155); stroke-width: 2;\" ";
+	cmds += "stroke-dasharray=\"5, 5\" x1=\""+prevX+"\" y1=\""+prevY+"\" x2=\""+nextX+"\" y2=\""+nextY+"\"></line>\n";	
+	cmds += "<circle cx=\""+nextX+"\" cy=\""+nextY+"\" r=\"5\" fill=\"green\"/>\n";
+    }
+    if(operation.match(/m/i)) { // for move show previous, next coords
+	cmds += "<circle cx=\""+prevX+"\" cy=\""+prevY+"\" r=\"5\" fill=\"red\"/>\n";
+	cmds += "<line style =\"stroke:rgb(155, 155, 155); stroke-width: 2;\" ";
+	cmds += "stroke-dasharray=\"5, 5\" x1=\""+prevX+"\" y1=\""+prevY+"\" x2=\""+nextX+"\" y2=\""+nextY+"\"></line>\n";
+	cmds += "<circle cx=\""+nextX+"\" cy=\""+nextY+"\" r=\"5\" fill=\"green\"/>\n";
+    }
+    if(operation.match(/c/i)) { // for cubic curve, show previous, next points, with control points (in gray)
+	cmds += "<circle cx=\""+prevX+"\" cy=\""+prevY+"\" r=\"5\" fill=\"red\"/>\n";	
+	cmds += "<circle cx=\""+params[0]+"\" cy=\""+params[1]+"\" r=\"5\" fill=\"gray\"/>\n";
+	cmds += "<line style =\"stroke:rgb(155, 155, 155); stroke-width: 2;\" ";
+	cmds += "stroke-dasharray=\"5, 5\" x1=\""+prevX+"\" y1=\""+prevY+"\" x2=\""+params[0]+"\" y2=\""+params[1]+"\"></line>\n";	
+	cmds += "<circle cx=\""+params[2]+"\" cy=\""+params[3]+"\" r=\"5\" fill=\"gray\"/>\n";
+	cmds += "<circle cx=\""+nextX+"\" cy=\""+nextY+"\" r=\"5\" fill=\"green\"/>\n";
+	cmds += "<line style =\"stroke:rgb(155, 155, 155); stroke-width: 2;\" ";
+	cmds += "stroke-dasharray=\"5, 5\" x1=\""+params[2]+"\" y1=\""+params[3]+"\" x2=\""+nextX+"\" y2=\""+nextY+"\"></line>\n";	
+    }
+    if(operation.match(/z/i)) {
+	
+    }
+    return cmds;
 }
 
 function getTracerButtonSrc(fName, value){
@@ -99,8 +210,11 @@ function codeDisplay(idx){
     }
     srcElem.innerHTML = "<pre class=\"executed\">" + executedCode + "</pre>";
     srcElem.innerHTML += "<pre class=\"toExec\">" + toExecute + "</pre>";
+    // Having set the source code for display, we now set what is to be shown ...
+    let targetCode = "<path d=\"" + executedCode + "\"></path>" + infoL[idx];
+    
     let targetElem = document.getElementById('bViews-symbolDisplayed');
-    targetElem.innerHTML = "<path d=\"" + executedCode + "\"></path>";
+    targetElem.innerHTML = targetCode;
 }
 
 function getExecutedCode(commandSeq, idL, idx){
